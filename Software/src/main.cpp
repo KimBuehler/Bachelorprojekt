0fbdb33c0011632027082c8d3775ce5983c51132
KimBuehler <buehki01@thu.de>
2023-11-25 13:07:26 +0100

    Erstellen der Funktion VentilBetaetigen
    kleine Anpassungen bei den Funktionen des ULtraschall Sensors
    kleine Anpassungen bei den Funktionen des Schrittmotors

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <UltraschallSensor>
 #include <Schrittmotor>
-#include <Stepper.h> 
+#include <Ventil>
 
 // UltraschallSensor
 #define PIN_TRIGGER 12
@@ -9,40 +9,46 @@
 unsigned long Geschwindigkeit; // in m/s
 
 // Schrittmotor
-#define STEPS 200 // Aus Datenblatt entnehmen, Schrittanzahl für 360°
-int Winkel = 30; // in °, Nullebene ist der Boden
+#define STEPS 2000 // Aus Datenblatt entnehmen, Schrittanzahl für 360°
+#define PIN_ENABLE 6
+#define PIN_STEP 5
+#define PIN_DIRECTION 4
+int Winkel = 30; // in °, Nullebene ist parallel zum Boden
+
+// Ventil
+#define PIN_RELAIS 8
+
 
 void setup() {
   Serial.begin(9600);
+
+  // Einrichten Ultraschallsensor-Pins
   pinMode(PIN_TRIGGER, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
   
-  //Einrichten Motor-Pins
-  pinMode(6, OUTPUT); //Enable
-  pinMode(5,OUTPUT); //Step
-  pinMode(4, OUTPUT); //direction
-  digitalWrite(6, LOW);
+  // Einrichten Motor-Pins
+  pinMode(PIN_ENABLE, OUTPUT);
+  pinMode(PIN_STEP,OUTPUT); 
+  pinMode(PIN_DIRECTION, OUTPUT); 
+  digitalWrite(PIN_ENABLE, LOW);
+
+  //Einrichten Ventil-Pin
+  pinMode(PIN_RELAIS, OUTPUT);
 
 }
 
 void loop() { 
-  digitalWrite(4, LOW);
-    for(int stepCounter = 0; stepCounter < 2000; stepCounter++) {
-      digitalWrite(5,HIGH);
-    delayMicroseconds(5000);
-    digitalWrite(5,LOW);
-    delayMicroseconds(5000);
-    }
-delay(1000);
-  //Winkeleinstellung(Winkel);
-  //Serial.println("Eingestellter Winkel: " + Winkel);
+  Winkeleinstellung(Winkel, STEPS, PIN_DIRECTION, PIN_STEP);
+  Serial.println("Eingestellter Winkel: " + Winkel);
   Winkel = Winkelueberpruefung();
-  //Serial.println("Vorhandener Winkl: " + Winkel); 
+  Serial.println("Vorhandener Winkel: " + Winkel); 
+
+  //VentilBetaetigen (PIN_RELAIS);
   do{
   bool Ball_erkannt = Ballerkennung(PIN_TRIGGER, PIN_ECHO);
   //Serial.println(Ball_erkannt);
   Geschwindigkeit = Geschwindigkeitsmessung(Ball_erkannt); // m/s
   } while (Ball_erkannt==true);
-  //Serial.println(Geschwindigkeit);
+  Serial.println("Geschwindigkeit: " + Geschwindigkeit);
 }
 
