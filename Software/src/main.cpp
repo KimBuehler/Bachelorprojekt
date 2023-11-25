72c1adc3a7a78aa40d32665fd0f6d55f7a727c46
KimBuehler <buehki01@thu.de>
2023-11-25 11:34:30 +0100

    Testversion erstes Mal Schaltung aufgebaut
    US Sensor sendet Daten und unterscheidet ob etwas im Sichtfeld ist oder nicht, Geschwindigkeit wird noch nicht richtig ausgegeben
    
    Schrittmotor hat ein Haltemoment, die HIGH und LOW Level kommen am Treiber an, werden aber noch nicht in Bewegungen am Schrittmotor umgewandelt

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -10,25 +10,39 @@ unsigned long Geschwindigkeit; // in m/s
 
 // Schrittmotor
 #define STEPS 200 // Aus Datenblatt entnehmen, Schrittanzahl für 360°
-Stepper stepper(STEPS, 8, 9, 10, 11);
 int Winkel = 30; // in °, Nullebene ist der Boden
 
 void setup() {
   Serial.begin(9600);
   pinMode(PIN_TRIGGER, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
-  stepper.setSpeed(30); // in Umdrehungen/min
+  
+  //Einrichten Motor-Pins
+  pinMode(6, OUTPUT); //Enable
+  pinMode(5,OUTPUT); //Step
+  pinMode(4, OUTPUT); //direction
+  digitalWrite(6, LOW);
+
 }
 
 void loop() { 
-  Winkeleinstellung(Winkel, stepper);
-  Serial.print("Eingestellter Winkel: " + Winkel);
+  digitalWrite(4, LOW);
+    for(int stepCounter = 0; stepCounter < 2000; stepCounter++) {
+      digitalWrite(5,HIGH);
+    delayMicroseconds(5000);
+    digitalWrite(5,LOW);
+    delayMicroseconds(5000);
+    }
+delay(1000);
+  //Winkeleinstellung(Winkel);
+  //Serial.println("Eingestellter Winkel: " + Winkel);
   Winkel = Winkelueberpruefung();
-  Serial.print("Vorhandener Winkl: " + Winkel); 
+  //Serial.println("Vorhandener Winkl: " + Winkel); 
   do{
   bool Ball_erkannt = Ballerkennung(PIN_TRIGGER, PIN_ECHO);
+  //Serial.println(Ball_erkannt);
   Geschwindigkeit = Geschwindigkeitsmessung(Ball_erkannt); // m/s
   } while (Ball_erkannt==true);
-  Serial.print(Geschwindigkeit);
+  //Serial.println(Geschwindigkeit);
 }
 
